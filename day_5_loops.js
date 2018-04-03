faee8c9517be228e52d2c76db1b393f05a3c9753
aaronsato <2asato@gmail.com>
2018-04-02 23:34:30 -0700

    created file

---
+++ day_5_loops.js
@@ -0,0 +1,6 @@
+// Given an integer,n, print its first 10 multiples. Each multiple n * i (where ) should be printed on a new line in the form: n x i = result.
+
+function main() {
+    var n = parseInt(readLine());
+
+}
