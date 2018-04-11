7b1f7ad562e3d5069f2ced0106a478f610145b1b
aaronsato <2asato@gmail.com>
2018-04-11 11:16:39 -0700

    comments

--- day_9_recursion.js
+++ day_9_recursion.js
@@ -2,6 +2,8 @@ function factorial(n) {
     // Complete this function
     let result = 1;
     for (let i = n; i > 0; i--) {
+      // starts at n and goes down till 1
+      // takes each number and multiples it by the next
       result *= i;
     }
     return result;
